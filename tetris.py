08fb4ba2384c468279dd87955925615a74318713
Unscribed1 <alxmoc96@gmail.com>
2024-07-27 14:51:59 +0100

    update

--- tetris.py
+++ tetris.py
@@ -38,12 +38,15 @@ class square():
         self.a = a
         self.b = b
         self.occupier = color # color
+        self.outl = "white"
         w = 0.5
-        if self.occupier != "black":
+        if self.occupier == "#dde5e3":
+            self.outl = "grey"
+            w = 2
+        elif self.occupier != "#bfb7b6":
             w = 1
-        self.shape = canvas.create_rectangle(a,b,a+25,b+25, fill=self.occupier, width=w, outline = "white")    
-       
-        
+            self.outl = "black"
+        self.shape = canvas.create_rectangle(a,b,a+25,b+25, fill=self.occupier, width=w, outline = self.outl)         
         
 canvas = Canvas(interface, bg = 'white', width = 600, height = 600)
 canvas.pack()
@@ -76,9 +79,6 @@ def key_guide(curr_window, direction):
     if curr_window == escape_window:
         key_escape(curr_window, direction)
 
-# t1=threading.Thread(target=key_guide)
-# t1.start()
-
 def toggle_escape_window(curr_window, direction):
     global game_paused
     if escape_window.active == False:             
@@ -94,12 +94,10 @@ def toggle_escape_window(curr_window, direction):
         change_window(prev_window)
         escape_window.active = False
         game_paused = False
-        # g = threading.Thread(target=game_loop, args=()) # threading allows us to input while the loop is being ran. If I use just game_loop(), it won't work
-        # g.start()
         return
 
 def key_game(curr_window, direction):
-    global current_stamper
+    global current_stamper, spacecounter
     print("keygame1")
     if direction == "escape":
         toggle_escape_window(curr_window, direction)
@@ -109,6 +107,10 @@ def key_game(curr_window, direction):
         stamp_lr(current_stamper, -25)
     elif direction == "down":
         stamp_lower(current_stamper)
+    elif direction == "space":
+        if spacecounter == 0:
+            spacecounter = 10
+            instant_lower()
         
 def key_highscore(curr_window, direction):
     if direction == "escape":
@@ -155,13 +157,45 @@ def select(curr_window, selector):
             print("THIS IS IT",curr_window.buttonlist[i].name)
             bring_to_front(curr_window.buttonlist[i].tied_window)
             change_window(curr_window.buttonlist[i].tied_window)
-            
+
+def cleaner():
+    global gamewindow
+    for i in gamewindow.d:
+        canvas.delete(gamewindow.d[i].shape)
+        del i
+    gamewindow.d = {}
+    for r in range(1,21):
+        for c in range(1,11):
+            gamewindow.d["sq_{0}_{1}".format(r,c)] = square(r,c,150+(c*25),40+(r*25), "#bfb7b6")
+    for i in gamewindow.stamper:
+        print(i)
+        canvas.delete(gamewindow.stamper[i].shape)
+    for i in gamewindow.shadow:
+        print(i)
+        canvas.delete(gamewindow.shadow[i].shape)
+    gamewindow.shadow = ""
+    gamewindow.stamper = ""
+    return
+
+def cleaner2():
+    global gamewindow
+    for i in gamewindow.stamper:
+        print(i)
+        canvas.delete(gamewindow.stamper[i].shape)
+    for i in gamewindow.shadow:
+        print(i)
+        canvas.delete(gamewindow.shadow[i].shape)
+    gamewindow.shadow = ""
+    gamewindow.stamper = ""
+    return    
+         
 
 def change_window(new_win):
-        global curr_window, prev_window, game_started, game_paused, piece_queue
+        global curr_window, prev_window, game_started, game_paused, piece_queue, escape_window
         prev_window = curr_window
         curr_window = new_win
         if curr_window == mainmenu:
+            cleaner()
             game_started = False
             game_paused = True
             suppress_grid()
@@ -194,6 +228,9 @@ def bring_to_front(window):
                 # canvas.tag_raise(window.d[i].shape)
             for x in value:
                 canvas.tag_raise(value[x].shape)
+        elif key == "shadow":
+            for i in value:
+                canvas.tag_raise(window.shadow[i].shape)
         elif key == "stamper":
             for i in value:
                 canvas.tag_raise(window.stamper[i].shape)
@@ -218,7 +255,7 @@ def delete_message(inv, txt):
     canvas.delete(inv, txt)
 
 def game_loop():
-    global interface, game_paused, piece_queue, counter, current_piece, current_stamper, gamewindow
+    global interface, game_paused, piece_queue, counter, current_piece, current_stamper, gamewindow, spacecounter
     while True:
         if force_exit == 1:
             return
@@ -230,13 +267,16 @@ def game_loop():
             # print("pause")
             while game_paused == False and force_exit == 0: # game being ran
                 counter -= 1
-                while len(piece_queue) < 4:
+                if spacecounter > 0:
+                    spacecounter += -1
+                while len(piece_queue) < 5:
                     print("appending to piece list")
                     piece_queue_handler(piece_queue)
                 if current_piece == False:
                     current_piece = piece_queue[0]
                     piece_queue.pop(0)
-                    stamp_placer(current_piece) # Shelved
+                    stamp_placer(current_piece)
+                    piece_queue_drawer()
                 print(counter)
                 # board_updater() # Shelved
                 time.sleep(0.01)
@@ -249,18 +289,79 @@ def game_loop():
                 print("breaking")
                 clear_state()
 
+def piece_queue_drawer():
+    global piece_queue, gamewindow
+    y = 110
+    xx = -50
+    for i in gamewindow.q:
+        for t in range(len(gamewindow.q)):
+            for d in range(0,4):
+                canvas.delete(gamewindow.q[t]["block_{0}".format(d)].shape)
+    gamewindow.q = []
+    for p in piece_queue:
+        cr_coords = pieces_d[p] # cr_coords = value. Value being a list containing 4 lists
+        block_color = color_assign(p)
+        if block_color == "cyan":
+            xx = -62
+        elif block_color == "yellow":
+            xx = -60
+        elif (block_color != "cyan") or (block_color != "yellow"):
+            xx = -50
+        queueobj = {}
+        for i in range(0,4):
+            r = cr_coords[i][0]
+            c = cr_coords[i][1]
+            queueobj["block_{0}".format(i)] = square(r,c,xx+(c*25),y+(r*25), block_color)
+        for x in queueobj:
+            print(x)
+            canvas.tag_raise(queueobj[x].shape)
+        gamewindow.q.append(queueobj)
+        y += 100
+    
+    # cr_coords = pieces_d[current_piece] # cr_coords = value. Value being a list containing 4 lists
+    # block_color = color_assign(current_piece)
+    # shadow_stamper = {}
+    # for i in range(0,4):
+        # print("APPENDING STAMPER")
+        # r = cr_coords[i][0]
+        # c = cr_coords[i][1]
+        # current_stamper["block_{0}".format(i)] = square(r,c,150+(c*25),40+(r*25), block_color)
+        # shadow_stamper["block_{0}".format(i)] = square(r,c,150+(c*25),40+(r*25), "#dde5e3")
+    # stamp_shadow(shadow_stamper)
+    # gamewindow.stamper = current_stamper
+    # for x in gamewindow.stamper:
+        # canvas.tag_raise(gamewindow.stamper[x].shape)
+        
 def stamp_lower(current_stamper):
+    allignment_checker()
     for x in current_stamper:
-        canvas.moveto(current_stamper[x].shape, current_stamper[x].a, current_stamper[x].b+25)
         current_stamper[x].b += 25
+        canvas.moveto(current_stamper[x].shape, current_stamper[x].a, current_stamper[x].b)
 
+def instant_lower():
+    global gamewindow, current_stamper
+    for x in current_stamper:
+        current_stamper[x].b = gamewindow.shadow[x].b
+        current_stamper[x].a = gamewindow.shadow[x].a
+        canvas.moveto(current_stamper[x].shape, gamewindow.shadow[x].a, gamewindow.shadow[x].b)
+    allignment_checker()
+        
 def stamp_lr(current_stamper, deltax):
+    global gamewindow
     for i in range(0,4):        
         if ( (current_stamper["block_{0}".format(i)].a + deltax) < 175 ) or ( (current_stamper["block_{0}".format(i)].a + deltax) > 400 ):
             return
     for x in current_stamper:
         canvas.moveto(current_stamper[x].shape, current_stamper[x].a+deltax, current_stamper[x].b)
         current_stamper[x].a += deltax
+    for x in gamewindow.shadow:
+        canvas.moveto(gamewindow.shadow[x].shape, gamewindow.shadow[x].a+deltax, gamewindow.shadow[x].b)
+        gamewindow.shadow[x].a += deltax
+
+def stamp_rotate(current_stamper, direction):
+    global gamewindow
+    
+    
     
 def clear_state():
     global gamewindow, current_stamper, piece_queue, current_piece, game_started
@@ -274,39 +375,47 @@ def stamp_placer(current_piece):
     # gamewindow.d["sq_{0}_{1}".format(r,c)] = square(r,c,150+(c*25),40+(r*25))
     cr_coords = pieces_d[current_piece] # cr_coords = value. Value being a list containing 4 lists
     block_color = color_assign(current_piece)
+    shadow_stamper = {}
     for i in range(0,4):
         print("APPENDING STAMPER")
         r = cr_coords[i][0]
         c = cr_coords[i][1]
         current_stamper["block_{0}".format(i)] = square(r,c,150+(c*25),40+(r*25), block_color)
+        shadow_stamper["block_{0}".format(i)] = square(r,c,150+(c*25),40+(r*25), "#dde5e3")
+    stamp_shadow(shadow_stamper)
     gamewindow.stamper = current_stamper
     for x in gamewindow.stamper:
-        canvas.tag_raise(current_stamper[x].shape)
+        canvas.tag_raise(gamewindow.stamper[x].shape)
 
-
-def stamp_carrier():
-    pass
-
-# def piece_placer(current_piece): # old version, Shelved
-    # global pieces_d, gamewindow
-    # print("current piece",current_piece)
-    # block_color = color_assign(current_piece)
-    # print(pieces_d[current_piece])
-    # print(type(pieces_d[current_piece]))
-    # for x in pieces_d[current_piece]:
-        # x.occupier = block_color
-
-# def board_updater(): #I'll keep this shelved for now, not the best solution
-    # global gamewindow, canvas
-    # for i in gamewindow.d:
-        # canvas.delete(gamewindow.d[i].shape) 
-        # gamewindow.d[i].shape = canvas.create_rectangle(gamewindow.d[i].a,gamewindow.d[i].b,gamewindow.d[i].a+25,gamewindow.d[i].b+25, fill=gamewindow.d[i].occupier, width=0.5, outline = "white")
-        # canvas.itemconfig(gamewindow.d[i], fill=gamewindow.d[i].occupier)
-        # canvas.update_idletasks
-        # print(gamewindow.d[i].occupier)
+def allignment_checker(): # checks if the stamper and shadow are alligned
+    global gamewindow, counter, current_piece
+    to_stamp = []
+    if (gamewindow.shadow["block_0"].a == gamewindow.stamper["block_0"].a) and (gamewindow.shadow["block_0"].b == gamewindow.stamper["block_0"].b):
+        for i in gamewindow.shadow:
+            for f in gamewindow.d:
+                if (gamewindow.shadow[i].a == gamewindow.d[f].a) and (gamewindow.shadow[i].b == gamewindow.d[f].b):
+                    print("MATCH FOUND")
+                    print(gamewindow.d[f].occupier)
+                    gamewindow.d[f].occupier = gamewindow.stamper[i].occupier
+                    print(gamewindow.d[f].occupier)
+                    canvas.delete(gamewindow.d[f].shape)
+                    gamewindow.d[f].shape = canvas.create_rectangle(gamewindow.d[f].a,gamewindow.d[f].b,gamewindow.d[f].a+25,gamewindow.d[f].b+25, fill=gamewindow.d[f].occupier, width=0.25, outline = "black")
+        counter = 130
+        current_piece = False
+        cleaner2()
+        
+def stamp_shadow(shadow):
+    global gamewindow
+    gamewindow.shadow = shadow
+    if gamewindow.shadow["block_0"].b == gamewindow.shadow["block_1"].b == gamewindow.shadow["block_2"].b == gamewindow.shadow["block_3"].b:
+        for i in gamewindow.shadow: 
+            gamewindow.shadow[i].b += 25 # If the piece is I, it gets placed a row lower
+    for i in gamewindow.shadow:
+        gamewindow.shadow[i].b += 450
+        canvas.moveto(gamewindow.shadow[i].shape, gamewindow.shadow[i].a, gamewindow.shadow[i].b)
     
 def color_assign(current_piece):
-    colorlist = ["cyan", "blue", "orange", "yellow", "green", "violet", "red"]
+    colorlist = ["cyan", "blue", "orange", "yellow", "green", "#bc8ad0", "red"]
     choicelist = ["piece_I", "piece_J", "piece_L", "piece_O", "piece_S", "piece_T", "piece_Z"]
     index = choicelist.index(current_piece)
     color = colorlist[index]
@@ -329,6 +438,7 @@ gamewindow.upnext_text = canvas.create_text(85,105,text="UP NEXT", font=("Arial"
 gamewindow.level = 1
 gamewindow.level_text = canvas.create_text(300,30,text=("Lv.",gamewindow.level), font=("Arial", 24))
 gamewindow.score = 0
+gamewindow.q = []
 gamewindow.score_container = canvas.create_rectangle((450,120,570,150), fill="black", width = 0.5, outline = "white")
 gamewindow.score_text = canvas.create_text(510,105,text="SCORE", font=("Arial", 12))
 gamewindow.score_display = canvas.create_text(510,135,text=gamewindow.score, font=("Arial", 12),  fill = "white")
@@ -337,7 +447,7 @@ gamewindow.score_display = canvas.create_text(510,135,text=gamewindow.score, fon
 gamewindow.d = {}
 for r in range(1,21):
     for c in range(1,11):
-        gamewindow.d["sq_{0}_{1}".format(r,c)] = square(r,c,150+(c*25),40+(r*25), "black")
+        gamewindow.d["sq_{0}_{1}".format(r,c)] = square(r,c,150+(c*25),40+(r*25), "#bfb7b6")
 
 # Windows - High Score
 highscore = class_window("highscore","green",0,0,600,600)
@@ -376,6 +486,7 @@ game_paused = False
 piece_queue = []
 current_piece = False
 force_exit = 0
+spacecounter = 10
 counter = 130
 
 bring_to_front(mainmenu)
@@ -420,7 +531,7 @@ interface.bind("<Right>", lambda x: key_guide(curr_window, "right"))
 interface.bind("<Left>", lambda x: key_guide(curr_window, "left"))
 interface.bind("<Up>", lambda x: key_guide(curr_window, "up"))
 interface.bind("<Down>", lambda x: key_guide(curr_window, "down"))
-
+interface.bind("<space>", lambda x: key_guide(curr_window, "space"))
 # Game Loop
 g = threading.Thread(target=game_loop, args=()) # threading allows us to input while the loop is being ran. If I use just game_loop(), it won't wor
 g.start()
